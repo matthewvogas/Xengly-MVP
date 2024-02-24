cd81deb8e39f10c45fcbcbcf9d031803da6377cd
Catalina Cobaleda <catalinacobaleda@Catalinas-MacBook-Air.local>
2024-02-24 12:36:24 -0600

    components basics and storybook

---
+++ src/components/input/Input.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Input from './input.jsx';
+
+export default {
+  title: 'Input',
+  component: Input,
+};
+
+export const InputGeneral = () => <Input texto="Hola, Mundo!" />;
