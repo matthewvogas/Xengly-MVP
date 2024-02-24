f994fe4c1af5338b61fd68fdb54ddded370d332f
Luis Montero <lmonterom879@gmail.com>
2024-02-24 17:02:31 -0600

    buttons and some inputs finished

--- src/components/input/Input.stories.jsx
+++ src/components/input/Input.stories.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import Input from './input.jsx';
+import React from "react";
+import Input from "./input.jsx";
 
 export default {
-  title: 'Input',
+  title: "Input",
   component: Input,
 };
 
-export const InputGeneral = () => <Input texto="Hola, Mundo!" />;
+export const DefaultInput = () => (
+  <Input
+    className="input--container"
+    placeholder="placeholder"
+    text="placeholder"
+  />
+);
+
+// export const SearchInput = () => (
+//   <Input
+//     icon={Search}
+//     className="input--container"
+//     placeholder="placeholder"
+//     text="placeholder"
+//   />
+// );
--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import "../input/input.css";
 
-const Input = ({ texto }) => {
-  return <input type="text" />;
+const Input = ({ className, type, placeholder, icon: Icon }) => {
+  return (
+    <div>
+      <input className={className} type={type} placeholder={placeholder} />
+      {Icon && <Icon />}
+    </div>
+  );
 };
 
 export default Input;
