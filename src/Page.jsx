ee09eedb51896b0977b73e74e73192f48e9c4aac
Matthew <burgerqueen@xengly.com>
2024-02-29 14:37:04 -0600

    paths on menu

--- src/Page.jsx
+++ src/Page.jsx
@@ -8,6 +8,14 @@ import "./Page.css";
 import Menu from "./components/molecules/menu/menu";
 
 // routes
+
+// icons
+import { ReactComponent as discover } from "../src/assets/icons/menu/home.svg";
+import { ReactComponent as notifications } from "../src/assets/icons/menu/bell.svg";
+import { ReactComponent as meets } from "../src/assets/icons/menu/coffee.svg";
+import { ReactComponent as messages } from "../src/assets/icons/menu/message-square.svg";
+import { ReactComponent as settings } from "../src/assets/icons/menu/user.svg";
+
 import Notifications from "./pages/notifications";
 import Checkout from "./pages/checkout";
 import Discover from "./pages/discover";
@@ -15,20 +23,26 @@ import Settings from "./pages/settings";
 import Xengler from "./pages/xengler";
 
 // export
+const routes = [
+  { path: "/", Component: Discover, name: "Discover", icon: discover },
+  { path: "/notifications", Component: Notifications, name: "Notifications", icon: notifications },
+  { path: "/settings", Component: Settings, name: "Settings", icon: settings },
+  // { path: "/xengler", Component: Xengler, name: "Xengler", icon: meets },
+  // { path: "/checkout", Component: Checkout, name: "Checkout", icon: messages },
+];
+
 const App = () => (
   <div className="layout">
     <div className="sidebar">
-      <Menu />
+      <Menu routes={routes} />
     </div>
 
     <div className="content">
       <div className="module">
         <Routes>
-          <Route index Component={Discover} />
-          <Route path="settings" Component={Settings} />
-          <Route path="xengler" Component={Xengler} />
-          <Route path="notifications" Component={Notifications} />
-          <Route path="checkout" Component={Checkout} />
+          {routes.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
         </Routes>
       </div>
     </div>
