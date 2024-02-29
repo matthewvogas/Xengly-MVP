25602c6c03dedfe55ed8a2ca2dc44b5d90d67b22
Matthew <burgerqueen@xengly.com>
2024-02-29 10:01:36 -0600

    xengler page without images

--- src/components/organisms/creator/creator.jsx
+++ src/components/organisms/creator/creator.jsx
@@ -1,10 +1,81 @@
 import React from "react";
+import ImageExample from "../../../assets/images/imageProfile.jpg";
+import Image from "../../atoms/imageProfile/image";
+import ImageExampleForBio from "../../../assets/images/exampleImage.jpg";
+import Label from "../../atoms/labels/label";
+import Button from "../../atoms/button/button";
+import "./creator.css";
 
-const CreatorOrganism = ({ }) => {
-
+const CreatorOrganism = ({ Creator }) => {
+  const items = ["Escucha Activa", "Buen Conversador", "Valorant"];
   return (
     <section>
-      {/* layout de Creator */}
+      <div className="head--creator">
+        <div className="head--image">
+          <Image className="ImageProfile" profileImage={ImageExample} />
+        </div>
+        <div className="creators--head">
+          <div className="creator--titles">
+            <Label className="Title" text="Steve, 24" />
+            <Label className="SubTitleText" text="Influencer" />
+          </div>
+
+          <div className="creators-cta">
+            <Button text="Seguir" className="button--cancel" />
+            <Button text="Agendar Encuentro" className="button--primary" />
+          </div>
+        </div>
+      </div>
+
+      <div className="creator--content">
+        <div className="creator--bio">
+          <Label className="SubTitle" text="Biography" />
+          <Label
+            className="SubTitleText"
+            text="Steve likes spending time with his friend and planning travel with them at least once every year. Steve always prepared the trip very well, from research the destination until the budget preparation. "
+          />
+          <div className="images">
+            <p>images</p>
+          </div>
+        </div>
+
+        <div className="creator--info">
+          <div className="cta">
+            <img src={ImageExampleForBio} alt="" />
+            <div className="info--content">
+              <div className="pricing">
+                <Label className="SubTitle" text="20$" />
+                /
+                <Label className="SubTitleText" text="hour" />
+              </div>
+
+              <ul className="items">
+                {items.map((item, index) => (
+                  <li key={index}>· {item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="info">
+            <div className="info--content">
+              <div className="pricing">
+                <Label className="SubTitle" text="Historial" />
+              </div>
+
+              <div className="history">
+                <Label className="SubTitle" text="+40" />
+                <Label className="SubTitleText" text="fuera de xengly" />
+              </div>
+
+              <div className="history">
+                <Label className="SubTitle" text="+20" />
+                <Label className="SubTitleText" text="por medio de xengly" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
