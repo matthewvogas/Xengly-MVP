ccaa805313187015b1ee94fb930176b35990471f
Matthew <burgerqueen@xengly.com>
2024-03-11 22:46:26 -0600

    maps almost working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./Page";
 
+const apiKeyMaps = process.env.API_KEY_MAPS;
+<script
+  src={`https://maps.googleapis.com/maps/api/js?key=${apiKeyMaps}`}
+></script>;
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
